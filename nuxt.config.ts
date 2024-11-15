b2ab395a7957c3a39261a5357db03090076fd877
TristanCavite <tristan.cavite@gmail.com>
2024-11-15 21:45:11 +0800

    main

---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@samk-dev/nuxt-vcalendar",
+    "@vee-validate/nuxt",
+    "@morev/vue-transitions/nuxt"
+  ],
+
+  tailwindcss: {
+    exposeConfig: true,
+    editorSupport: true,
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  imports: {
+    imports: [{
+      from: "tailwind-variants",
+      name: "tv",
+    }, {
+      from: "tailwind-variants",
+      name: "VariantProps",
+      type: true,
+    }, {
+      from: "vue-sonner",
+      name: "toast",
+      as: "useSonner"
+    }],
+  },
+
+  build: {
+    transpile: ["vue-sonner"]
+  }
+});
